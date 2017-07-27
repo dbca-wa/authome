c28a669c7f3e8c1bd4c648ecd7b94ac6819006c2
Scott Percival <scott.percival@dpaw.wa.gov.au>
2017-07-27 15:08:37 +0800

    authome.tests: add some more bad credential tests

--- authome/test.py
+++ authome/test.py
@@ -9,6 +9,7 @@ import adal
 
 class AuthTestCase(TestCase):
     client = Client()
+    home_url = reverse('home')
     auth_url = reverse('auth')
     auth_ip_url = reverse('auth_ip')
     auth_dual_url = reverse('auth_dual')
@@ -21,6 +22,16 @@ class AuthTestCase(TestCase):
 
     def basic_auth(self, username, password):
         return 'Basic {}'.format(base64.b64encode('{}:{}'.format(username, password).encode('utf-8')).decode('utf-8'))
+
+#    @mock.patch('adal.AuthenticationContext.acquire_token_with_username_password')
+#    def test_home_redirects(self, mock_api_call):
+#        mock_api_call.return_value = {
+#            'userId': self.email
+#        }
+
+#        response = self.client.get(self.home_url)
+#        self.assertRedirects
+
 
     @mock.patch('adal.AuthenticationContext.acquire_token_with_username_password')
     def test_auth_adal_with_username(self, mock_api_call):
@@ -57,8 +68,31 @@ class AuthTestCase(TestCase):
         )
         self.assertEqual(response.status_code, 401)
 
-    def test_auth_adal_without_creds(self):
+    def test_auth_adal_with_bad_creds(self):
+        # no credentials
         response = self.client.get(self.auth_url)
+        self.assertEqual(response.status_code, 401)
+        # malformed Authorization Header
+        response = self.client.get(self.auth_url,
+            HTTP_AUTHORIZATION='Basic'
+        )
+        self.assertEqual(response.status_code, 401)
+        response = self.client.get(self.auth_url,
+            HTTP_AUTHORIZATION='Not a legit header'
+        )
+        self.assertEqual(response.status_code, 401)
+        response = self.client.get(self.auth_url,
+            HTTP_AUTHORIZATION='Basic 😭😭😭😕😕😕'
+        )
+        self.assertEqual(response.status_code, 401)
+        response = self.client.get(self.auth_url,
+            HTTP_AUTHORIZATION='Basic ==abcdef/+=='
+        )
+        self.assertEqual(response.status_code, 401)
+        # legit header, but invalid payload
+        response = self.client.get(self.auth_url,
+            HTTP_AUTHORIZATION='Basic '+base64.b64encode(b'notlegit').decode('utf-8')
+        )
         self.assertEqual(response.status_code, 401)
 
     @mock.patch('adal.AuthenticationContext.acquire_token_with_username_password')
@@ -116,5 +150,6 @@ class AuthTestCase(TestCase):
         self.assertIn('email', response.json())
         self.assertEqual(response.json()['email'], self.email)
 
-
-
+    def test_auth_dual_without_creds(self):
+        response = self.client.get(self.auth_dual_url)
+        self.assertEqual(response.status_code, 200)
