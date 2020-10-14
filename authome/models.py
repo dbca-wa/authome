cfb61d92e31c3006d2216cf4973089901ab7ee6b
Ashley Felton <ashley@ropable.com>
2020-10-14 11:58:46 +0800

    Remove unused import.

--- authome/models.py
+++ authome/models.py
@@ -1,13 +1,12 @@
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.contrib.auth.signals import user_logged_in
+from django.contrib.sessions.models import Session
 from django.core import management
 from django.utils import timezone
 from django.db import models
 from ipware.ip import get_client_ip
 import hashlib
 
-from django.contrib.sessions.models import Session
 
 class UserSession(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
@@ -26,5 +25,6 @@ def user_logged_in_handler(sender, request, user, **kwargs):
     usersession.ip = get_client_ip(request)
     usersession.save()
     management.call_command("clearsessions", verbosity=0)
+
 
 user_logged_in.connect(user_logged_in_handler)
