b8a64c47b03925938d9873b950443e1b635dbe0f
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2023-05-09 15:12:00 +0800

    Synchronize the user cache only after saving the user data into db

--- authome/forms/__init__.py
+++ authome/forms/__init__.py
@@ -3,4 +3,5 @@ from django.conf import settings
 from .forms import *
 
 if settings.AUTH2_CLUSTER_ENABLED:
-    from .clusterforms import UserEditForm
+    #from .clusterforms import UserEditForm
+    pass
